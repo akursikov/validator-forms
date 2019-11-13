61901df3863ee8b03f109e86c21f7ac91a8aaa5e
Alexander Kursikov <alexander.kursikov@gmail.com>
2019-11-12 21:02:56 -0500

    Reexport custom rule from the root index.js.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 export * from './rules';
-export * from './customRules';
+export { default as lengthBetween } from './customRules/lengthBetween';
+export { default as maxLength } from './customRules/maxLength';
+export { default as minLength } from './customRules/minLength';
+export { default as required } from './customRules/required';
 export * from './helpers';
